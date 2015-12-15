089f7553bf6b7a5735f51d89df6aa748c30b1b7f
xuchongbo <xucb@twshi>
2015-12-15 15:34:33 +0800

    change to port to 8000.

--- digits/utils/image.py
+++ digits/utils/image.py
@@ -241,14 +241,16 @@ def resize_image(image, height, width,
             noise_size = (padding, width)
             if channels > 1:
                 noise_size += (channels,)
-            noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            #noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            noise = np.full(noise_size,255,dtype='uint8')
             image = np.concatenate((noise, image, noise), axis=0)
         else:
             padding = (width - resize_width)/2
             noise_size = (height, padding)
             if channels > 1:
                 noise_size += (channels,)
-            noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            #noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            noise = np.full(noise_size,255,dtype='uint8')
             image = np.concatenate((noise, image, noise), axis=1)
 
         return image
